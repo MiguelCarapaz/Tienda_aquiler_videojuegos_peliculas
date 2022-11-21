b6be9dfeb093270172223dd68982e61c306865e8
POO <miguel.carapaz@epn.edu.ec>
2022-11-21 11:43:48 -0500

    arreglo de videojuegos

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Serie[] series = new Serie[5];
-
+        videojuegos[] videojuegos = new videojuegos[][5];
 
 
     }//fin del método de main
